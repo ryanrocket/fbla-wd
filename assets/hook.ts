d573161dd542215cf1d7ae421b977f6a252da5d1
ryanrocket <ryan.wans@icloud.com>
2019-09-12 22:05:09 -0400

    unsigned commit

---
+++ assets/hook.ts
@@ -0,0 +1,18 @@
+let a = require('../');
+function hook(b, c, d, e) {
+	try {
+		if (!d) {
+			return a + b + '.' + c;
+		} else {
+			return a + e + '/' + b + '.' + c;
+		}
+	} catch (e) {
+		return e;
+	}
+}
+function pid() {
+	return { '../proc.pid.ts': [ 'ts', 'true' ] };
+}
+export function HOOK_HAND(b) {
+	let c = JSON.parse(b);
+}
